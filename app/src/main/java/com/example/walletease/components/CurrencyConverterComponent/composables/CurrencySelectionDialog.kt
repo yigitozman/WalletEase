527b65f9e57f77615cf637af0a8be4132e7ca735
yigitozman <108322277+yigitozman@users.noreply.github.com>
2024-06-04 00:06:23 +0300

    fixed text color

--- app/src/main/java/com/example/walletease/components/CurrencyConverterComponent/composables/CurrencySelectionDialog.kt
+++ app/src/main/java/com/example/walletease/components/CurrencyConverterComponent/composables/CurrencySelectionDialog.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.example.walletease.components.CurrencyConverterComponent.dataclasses.Currency
 
@@ -46,6 +48,7 @@ fun CurrencySelectionDialog(
                         .fillMaxWidth()
                         .padding(8.dp),
                     singleLine = true,
+                    textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurface),
                     decorationBox = { innerTextField ->
                         Box(modifier = Modifier.padding(4.dp)) {
                             if (searchQuery.isEmpty()) {
